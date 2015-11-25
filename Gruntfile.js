d762ece04adb5aac840cc71b6e65103909b6f687
Masaru Furuya <m.furuya@comnico.jp>
2015-11-25 18:44:08 +0900

    load-grunt-configでタスクを分割

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,7 @@
 
 // wrapper function(実際の設定は全てこの中に書く)
 module.exports = function(grunt) {
-  // 設定情報を初期化
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    karma: {
-      unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true
-      }
-    }
-  });
+  require('load-grunt-config')(grunt);
   // package.jsonでインストールしたプラグインをロード
   grunt.loadNpmTasks('grunt-karma');
   // デフォルトタスク(grunt起動時に自動実行)
